6128973ddf8dc211c010ee182cfa74ebb30b9ca0
root <victor.bolheme@gmail.com>
2024-08-14 19:21:36 +0000

    + | Adding lines to the makefile

--- drivers/cpu/isr.s
+++ drivers/cpu/isr.s
@@ -97,187 +97,187 @@ irq_common_stub:
 isr0:
 	push	byte 0
 	push	byte 0
-	jmp		$isr_common_stub
+	jmp		isr_common_stub
 
 # 1: Debug Exception
 isr1:
 	push	byte 0
 	push	byte 1
-	jmp		$isr_common_stub
+	jmp		isr_common_stub
 
 # 2: Non Maskable Interrupt Exception
 isr2:
     push	byte 0
     push	byte 2
-    jmp		$isr_common_stub
+    jmp		isr_common_stub
 
 # 3: Int 3 Exception
 isr3:
     push	byte 0
     push	byte 3
-    jmp		$isr_common_stub
+    jmp		isr_common_stub
 
 # 4: INTO Exception
 isr4:
     push	byte 0
     push	byte 4
-    jmp		$isr_common_stub
+    jmp		isr_common_stub
 
 # 5: Out of Bounds Exception
 isr5:
     push	byte 0
     push	byte 5
-    jmp 	$isr_common_stub
+    jmp 	isr_common_stub
 
 # 6: Invalid Opcode Exception
 isr6:
     push	byte 0
     push	byte 6
-    jmp 	$isr_common_stub
+    jmp 	isr_common_stub
 
 # 7: Coprocessor Not Available Exception
 isr7:
     push	byte 0
     push	byte 7
-    jmp 	$isr_common_stub
+    jmp 	isr_common_stub
 
 # 8: Double Fault Exception (With Error Code!)
 isr8:
     push	byte 8
-    jmp 	$isr_common_stub
+    jmp 	isr_common_stub
 
 # 9: Coprocessor Segment Overrun Exception
 isr9:
     push	byte 0
     push	byte 9
-    jmp 	$isr_common_stub
+    jmp 	isr_common_stub
 
 # 10: Bad TSS Exception (With Error Code!)
 isr10:
     push 	byte 10
-    jmp 	$isr_common_stub
+    jmp 	isr_common_stub
 
 # 11: Segment Not Present Exception (With Error Code!)
 isr11:
     push	byte 11
-    jmp 	$isr_common_stub
+    jmp 	isr_common_stub
 
 # 12: Stack Fault Exception (With Error Code!)
 isr12:
     push	byte 12
-    jmp 	$isr_common_stub
+    jmp 	isr_common_stub
 
 # 13: General Protection Fault Exception (With Error Code!)
 isr13:
     push 	byte 13
-    jmp 	$isr_common_stub
+    jmp 	isr_common_stub
 
 # 14: Page Fault Exception (With Error Code!)
 isr14:
     push 	byte 14
-    jmp 	$isr_common_stub
+    jmp 	isr_common_stub
 
 # 15: Reserved Exception
 isr15:
     push 	byte 0
     push 	byte 15
-    jmp		$isr_common_stub
+    jmp		isr_common_stub
 
 # 16: Floating Point Exception
 isr16:
     push 	byte 0
     push 	byte 16
-    jmp		$isr_common_stub
+    jmp		isr_common_stub
 
 # 17: Alignment Check Exception
 isr17:
     push	byte 0
     push	byte 17
-    jmp 	$isr_common_stub
+    jmp 	isr_common_stub
 
 # 18: Machine Check Exception
 isr18:
     push	byte 0
     push	byte 18
-    jmp 	$isr_common_stub
+    jmp 	isr_common_stub
 
 # 19: Reserved
 isr19:
     push 	byte 0
     push 	byte 19
-    jmp 	$isr_common_stub
+    jmp 	isr_common_stub
 
 # 20: Reserved
 isr20:
     push 	byte 0
     push	byte 20
-    jmp 	$isr_common_stub
+    jmp 	isr_common_stub
 
 # 21: Reserved
 isr21:
     push 	byte 0
     push 	byte 21
-    jmp 	$isr_common_stub
+    jmp 	isr_common_stub
 
 # 22: Reserved
 isr22:
     push 	byte 0
     push 	byte 22
-    jmp 	$isr_common_stub
+    jmp 	isr_common_stub
 
 # 23: Reserved
 isr23:
     push 	byte 0
     push 	byte 23
-    jmp 	$isr_common_stub
+    jmp 	isr_common_stub
 
 # 24: Reserved
 isr24:
     push 	byte 0
     push 	byte 24
-    jmp 	$isr_common_stub
+    jmp 	isr_common_stub
 
 # 25: Reserved
 isr25:
     push 	byte 0
     push 	byte 25
-    jmp 	$isr_common_stub
+    jmp 	isr_common_stub
 
 # 26: Reserved
 isr26:
     push 	byte 0
     push 	byte 26
-    jmp 	$isr_common_stub
+    jmp 	isr_common_stub
 
 # 27: Reserved
 isr27:
     push	byte 0
     push	byte 27
-    jmp 	$isr_common_stub
+    jmp 	isr_common_stub
 
 # 28: Reserved
 isr28:
     push	byte 0
     push	byte 28
-    jmp 	$isr_common_stub
+    jmp 	isr_common_stub
 
 # 29: Reserved
 isr29:
     push 	byte 0
     push	byte 29
-    jmp 	$isr_common_stub
+    jmp 	isr_common_stub
 
 # 30: Reserved
 isr30:
     push 	byte 0
     push 	byte 30
-    jmp		$isr_common_stub
+    jmp		isr_common_stub
 
 # 31: Reserved
 isr31:
-    push	 byte 0
+    push	byte 0
     push 	byte 31
-    jmp 	$isr_common_stub
+    jmp 	isr_common_stub
 
 # IRQs (Interrupt Request)
 .global irq0
@@ -301,79 +301,79 @@ isr31:
 irq0:
 	push byte 0
 	push byte 32
-	jmp $irq_common_stub
+	jmp irq_common_stub
 
 irq1:
 	push byte 1
 	push byte 33
-	jmp $irq_common_stub
+	jmp irq_common_stub
 
 irq2:
 	push byte 2
 	push byte 34
-	jmp $irq_common_stub
+	jmp irq_common_stub
 
 irq3:
 	push byte 3
 	push byte 35
-	jmp $irq_common_stub
+	jmp irq_common_stub
 
 irq4:
 	push byte 4
 	push byte 36
-	jmp $irq_common_stub
+	jmp irq_common_stub
 
 irq5:
 	push byte 5
 	push byte 37
-	jmp $irq_common_stub
+	jmp irq_common_stub
 
 irq6:
 	push byte 6
 	push byte 38
-	jmp $irq_common_stub
+	jmp irq_common_stub
 
 irq7:
 	push byte 7
 	push byte 39
-	jmp $irq_common_stub
+	jmp irq_common_stub
 
 irq8:
 	push byte 8
 	push byte 40
-	jmp $irq_common_stub
+	jmp irq_common_stub
 
 irq9:
 	push byte 9
 	push byte 41
-	jmp $irq_common_stub
+	jmp irq_common_stub
 
 irq10:
 	push byte 10
 	push byte 42
-	jmp $irq_common_stub
+	jmp irq_common_stub
 
 irq11:
 	push byte 11
 	push byte 43
-	jmp $irq_common_stub
+	jmp irq_common_stub
 
 irq12:
 	push byte 12
 	push byte 44
-	jmp $irq_common_stub
+	jmp irq_common_stub
 
 irq13:
 	push byte 13
 	push byte 45
-	jmp $irq_common_stub
+	jmp irq_common_stub
 
 irq14:
 	push byte 14
 	push byte 46
-	jmp $irq_common_stub
+	jmp irq_common_stub
 
 irq15:
 	push byte 15
 	push byte 47
-	jmp $irq_common_stub
+	jmp irq_common_stub
