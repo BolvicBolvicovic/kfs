0dbd8c45c5f1235853214918c8baea8042d39b7b
Victor Cornille <victor.bolheme@gmail.com>
2024-08-18 22:25:01 +0200

    ~ | Test to put in evidence the issue

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -9,6 +9,6 @@ void	kernel_main() {
 	term_clear();
 
 	term_print("Hello, World!\0", 14);
-	char k[] = "Welcome to the Kernel\n";
-	printf("Here is a message:\n%s", k);
+	char k[] = "Welcome to the Kernel\nheeeeeeeelllllllllllll yeah";
+	printf("%sHere is a message:\n", k);
 }
