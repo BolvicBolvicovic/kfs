5b09ee6e555d9b5a76e4071be018e34f5aff3bcc
root <victor.bolheme@gmail.com>
2024-09-03 19:37:09 +0000

    + | PMM functionnal

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -25,11 +25,11 @@ extern uint32_t code;
 void	kernel_main(uint32_t magic, uint32_t addr) {
     
     multiboot_info_t* mbi = (multiboot_info_t*)addr;
-    uint32_t kheap_start = (uint32_t)&endkernel + 0x1000;
-    uint32_t kheap_end = kheap_start + 0x100000;
+    uint32_t kheap_start  = (uint32_t)&endkernel + 0x1000;
+    uint32_t kheap_end    = kheap_start + 0x100000;
     memory_region* region = (memory_region*) 0x1000;
-//    uint32_t mem_size = 1024 + mbi->mem_lower + mbi->mem_upper * 64;
-    uint32_t mem_size = 130816;
+    uint32_t mem_size = 1024 + mbi->mem_lower + mbi->mem_upper * 64;
+//    uint32_t mem_size     = 130816;
 
     init_current_screen(YELLOW, BLUE);
 	term_clear();
@@ -37,27 +37,13 @@ void	kernel_main(uint32_t magic, uint32_t addr) {
 	init_keyboard();
 	init_timer(50);
     pmm_init(mem_size, kheap_start);
-    printf("PMM initialized with %d KB\n", mem_size);
 
-    printf("\nPM map\n");
     for (size_t i = 0; i < 15; i++) {
         if (region[i].type > 4)              region[i].type = 1;
         if (i > 0 && region[i].startLo == 0) break;
-        printf("region %d: start high 0x%x s_low 0x%x length (bytes): 0x%x l_low 0x%x type: %d (%s)\n", i,
-            region[i].startHi, region[i].startLo,
-            region[i].sizeHi, region[i].sizeLo,
-            region[i].type, strMemoryTypes[region[i].type]
-        );
         if (region[i].type == 1) pmm_init_region(region[i].startLo, region[i].sizeLo);
     }
     pmm_deinit_region(0x10000, &endkernel - &code);
 	//initialise_paging();
 	asm volatile("sti\n\t");
-    uint32_t* p = (uint32_t*)pmm_allock_block();
-    printf("\np allocated at   0x%x\n", p);
-    pmm_free_block(p);
-    p = (uint32_t*)pmm_allock_block();
-    printf("p reallocated at 0x%x\n", p);
-    uint32_t* f = (uint32_t*)pmm_allock_block();
-    printf("f allocated at   0x%x\n", p);
 }
---
+++ memory/vmm.h
@@ -0,0 +1,6 @@
+#ifndef VMM_H
+#define VMM_H
+
+#include "../pmm/pmm.h"
+
+#endif
