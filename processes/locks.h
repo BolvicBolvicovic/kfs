f44ea07724336fab3bb3368d050f2d4790f903ab
Victor Cornille <victor.bolheme@gmail.com>
2025-12-08 15:43:13 +0100

    wip: untested mutexes

--- processes/locks.h
+++ processes/locks.h
@@ -22,6 +22,7 @@ spinlock_lock(spinlock_t* sl)
 {
 	while (atomic_cmpxchg((atomic_t*)sl, 0, 1))
 	{
+		// Note: if it was a user friendly implementation, I would use "pause" instead.
 		asm volatile ("hlt");
 	}
 }
@@ -43,13 +44,9 @@ static inline bool
 spinlock_try_lock(spinlock_t* sl)
 {
 	if (atomic_cmpxchg((atomic_t*)sl, 0, 1) == 0)
-	{
 		return 1;
-	}
 	else
-	{
 		return 0;
-	}
 }
 
 /* Name: SPINLOCK_DEFINE
@@ -124,4 +121,72 @@ semaphore_signal(semaphore_t* sem)
  * */
 #define	SEMAPHORE_DEFINE(name)		semaphore_t	name = {0,0,{0},{0}}
 
+/* Name: mutex_t
+ * Descrition: a binary semaphore that uses a boolean value instead of an integer.
+ * */
+typedef struct
+{
+	process_t*	head;
+	process_t*	tail;
+	spinlock_t	sl_list;
+	atomic_t	lock;
+} mutex_t;
+
+/* Name: mutex_lock
+ * Descrition: try to take the lock. If it is not available, puts the process on the waiting queue
+ * until the mutex is released by the process holding it.
+ * */
+static inline void
+mutex_lock(mutex_t* mu)
+{
+	if (atomic_cmpxchg((atomic_t*)sl, 0, 1))
+	{
+		spinlock_lock(&mu->sl_list);
+		scheduler_lock();
+
+		process_t*	current_process = current_process_pop();
+
+		if (!mu->head)
+			mu->head = current_process;
+		else
+			mu->tail->next = (u32)current_process;
+
+		mu->tail = current_process;
+
+		spinlock_unlock(&mu->sl_list);
+		scheduler_unlock();
+
+		// Note: trigger the scheduler
+		asm volatile ("int $1F");
+	}
+}
+
+/* Name: mutex_unlock
+ * Descrition: unlocks the mutext and indicates to the scheduler
+ * to wake the next waiting process in the queue.
+ * */
+static inline void
+mutex_unlock(mutex_t* mu)
+{
+	spinlock_lock(&mu->sl_list);
+
+	if (!mu->head)
+	{
+		new_process_list_push(mu->head);
+		mu->head = (process_t*)mu->head->next;
+	}
+	else
+	{
+		atomic_dec_and_test_neg(&mu->counter);
+	}
+
+	spinlock_unlock(&mu->sl_list);
+}
+
+/* Name: MUTEX_DEFINE
+ * Descrition: defines a mutex_t with input name.
+ * */
+#define	MUTEX_DEFINE(name)	mutex_t	name = {0,0,{0},{0}}
+
 #endif
+
